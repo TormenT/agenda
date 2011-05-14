bce2b90f33ec4dd6006c338092e9329c134611d4
Daniel TormenT <dantorment@gmail.com>
2011-05-14 00:43:13 -0300

    agenda

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,5 @@
+
+	    require "paperclip"
+
+    Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.5.6-Q8'
+    Paperclip.options[:swallow_stderr] = false
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = '4a822a1fad25c387e9b95c59b6f8600e8323113cbd6d2e472f3193fbff06c41525eaacaa22dd9e24fe6693e5a09ceae834c486df3aa758844a4f2cb620708ed7'
